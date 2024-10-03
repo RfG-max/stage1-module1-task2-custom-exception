04ea19a8fae6757da61a9ee25622039e960be212
RfG-max <chronolet@gmail.com>
2024-10-03 14:42:03 +0300

    Update StudentManager.java
    change find() method

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,9 +5,19 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
+  /*public Student find(long studentID) {
     return Student.getValueOf(studentID);
-  }
+  }*/
+
+  public Student find(long studentID) throws MyException {
+        Student student = Student.getValueOf(studentID);
+        if (student != null) {
+            return student;
+        } else {
+            throw new MyException(
+                    "Could not find student with ID " + studentID);
+        }
+    }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
